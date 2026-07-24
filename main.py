35c700b709d2ef7aacf0d6fdf7cb436a5011cf24
Aleksa Jojić <aleksajjc@gmail.com>
2026-07-24 12:33:29 +0200

    Stage 2: read endpoints with 404

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+
+tasks = [{"id":1,"title":"Buy groceries","done":False},
+         {"id":2,"title":"Sell groceries","done":False},
+         {"id":3,"title":"Food","done":True}]
 
 
 @app.get("/")
@@ -11,3 +16,16 @@ def describe():
 @app.get('/health')
 def check_health():
     return{"status":"ok"}
+
+
+@app.get('/tasks')
+def getAll():
+    return{"tasks":tasks}
+
+@app.get('/tasks/{id}')
+def getById(id: int):
+    for task in tasks:
+        if task["id"] == id:
+            return task
+    raise HTTPException(status_code=404, detail=f"Task {id} not found")
+
